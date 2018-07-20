82d66accf431d2d78bba0994fb116dfdb389a3e0
vartanbeno <vartanbeno@gmail.com>
2018-07-20 15:53:31 -0400

    list albums in /music path

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
