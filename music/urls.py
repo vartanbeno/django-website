3fc25534928eb650a5ac590d2ce9c4a6fe2c91dd
vartanbeno <vartanbeno@gmail.com>
2018-07-20 15:26:17 -0400

    create re_path containing album id

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- music/views.py
+++ music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello world</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
