e1621b10e9d96f83d7a8a9f5d77d345613a53740
vartanbeno <vartanbeno@gmail.com>
2018-07-20 16:46:30 -0400

    specify that we want the music's detail view, and not another app's

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
