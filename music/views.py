95c53e08b3981a31abafdc0a2627a44f37694fd1
vartanbeno <vartanbeno@gmail.com>
2018-07-20 16:17:41 -0400

    raise 404 http error if user tries to visit an album id which doesn't exist

--- music/views.py
+++ music/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = { 'all_albums': all_albums }
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', { 'all_albums': all_albums })
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album does not exist.')
+    return render(request, 'music/detail.html', { 'album': album })
