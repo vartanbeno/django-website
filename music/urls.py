ca9afb06760fa5885d6792af5261a0a2b8dc8a7f
vartanbeno <vartanbeno@gmail.com>
2018-07-23 21:06:26 +0100

    can now add albums to db

--- music/urls.py
+++ music/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 
     # /music/<album_id>
     re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add')
 ]
