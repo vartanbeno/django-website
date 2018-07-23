0acbec443da3164bbb09265dc5e977ed53fd1440
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-23 00:54:38 -0400

    add favourting functionality

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,10 @@ app_name = 'music'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
+
+    # /music/<album_id>
+    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favourite
+    re_path(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite')
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
@@ -8,3 +8,18 @@ def index(request):
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', { 'album': album })
+
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': 'You did not select a valid song.'
+        })
+    else:
+        selected_song.is_favourite = True if not selected_song.is_favourite else False
+        selected_song.save()
+        return render(request, 'music/detail.html', { 'album': album })
