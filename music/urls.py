b6fff38bdf6fb7355aada7320d6a88401e092204
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-23 23:40:27 -0400

    add user registration

--- music/urls.py
+++ music/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
     re_path(r'album/(?P<pk>[0-9]+)/update/', views.AlbumUpdate.as_view(), name='album-update'),
     re_path(r'album/(?P<pk>[0-9]+)/delete/', views.AlbumDelete.as_view(), name='album-delete'),
+
+    path('register/', views.UserFormView.as_view(), name='register')
     
 ]
